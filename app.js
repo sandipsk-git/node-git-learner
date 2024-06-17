ad8afacf69d2c2dbb51f4c7ff6a00cc33cd2f65a
Sandip Shivaji Kale <sandipsk.official@gmail.com>
2024-06-17 18:02:07 +0530

    port changed to 3001

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
